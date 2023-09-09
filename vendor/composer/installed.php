124d5058e76d1a950efe25302faa71feab9b0645
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-09-09 09:51:22 -0300

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7b7cfb6246fc59a39bf8354c8a79d3d35d7eae4f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7b7cfb6246fc59a39bf8354c8a79d3d35d7eae4f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'myfmbutler/myfmapilibrary-for-js' => array(
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
+            'reference' => '9b588f2d6ac177369c74615c4ad1569f62eab98a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../myfmbutler/myfmapilibrary-for-js',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
