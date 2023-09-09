6699f0bb102d1acf3e5744d30105e7eda023db6c
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-09-09 13:30:47 -0300

    init with errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,14 @@ import Image from 'next/image'
 import { useState } from 'react'
 import Header from '@/components/Header'
 import Form2Columns from '@/components/Form2Columns'
-import SideOverWithHeader from '@/components/SideOverWithHeader'
 import CalendarWithDayView from '@/components/CalendarWithDayView'
+import {Client} from 'fm-data-api-client';
+
+const client = new Client('https://server.selectjanitorial.com', 'Dev', 'NatureNeedsNurture', 'clarityData');
+const layout = client.layout('dapiOrganization');
+console.log(layout);
+const record = await layout.get(25);
+console.log(record);
 
 export default function Home({cleaner = "Niki", rate = 75}) {
   const [hoursBooked, setHoursBooked] = useState(2);
