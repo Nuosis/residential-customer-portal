0e58666caf997f3ab41d0904b5015344c922005b
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-09-09 12:46:31 -0300

    unable to access env and get token

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+console.log('Server from next.config.js', process.env.NEXT_PUBLIC_FM_SERVER);
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      // Fixes npm packages that depend on `fs` module
+        if (!isServer) {
+        config.resolve.fallback.fs = false;
+        }
+        return config;
+    }
+};
+console.log('Server from next.config.js');
 
 module.exports = nextConfig
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,32 @@ import { useState } from 'react'
 import Header from '@/components/Header'
 import Form2Columns from '@/components/Form2Columns'
 import CalendarWithDayView from '@/components/CalendarWithDayView'
+import { DataApi } from "@proofgeist/fmdapi";
+//import { fileTokenStore } from "@proofgeist/fmdapi/dist/tokenStore/file"; (BREAKS)
+//see methods at "https://github.com/proofgeist/fmdapi"
+//review modification to next.config.js
+
+console.log('userName',process.env.FM_USERNAME);
+console.log('password',process.env.FM_PASSWORD);
+console.log('db',process.env.FM_DATABASE);
+console.log('server',process.env.FM_SERVER);
+
+const client = DataApi({
+  auth: {
+    username: "Dev",
+    password: "NatureNeedsNurture",
+  },
+  db: "clarityData",
+  server: "https://server.selectjanitorial.com",
+});
+//console.log("dAPI Auth Result",client);
+
+const result = await client.list({ layout: "dapiOrganization" });
+console.log("dAPI Result",result);
 
 export default function Home({cleaner = "Niki", rate = 75}) {
   const [hoursBooked, setHoursBooked] = useState(2);
+  
   return (
     <div className="mt-2 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <Header/>
